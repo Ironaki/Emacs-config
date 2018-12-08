0c5b8e0bb7fdfa00844b491a66007724130f7940
Zixuan Li <armstrong0220@gmail.com>
2018-12-08 00:27:33 -0500

    update init.el

--- init.el
+++ init.el
@@ -1,30 +1,18 @@
 ;; use package
 (package-initialize)
 
-;; line number
-(global-linum-mode t) 
-
-;; highlight current line; not work in terminal
-;(when window-system         
-;  (global-hl-line-mode t)) 
-
-;; no startup msg  
-(setq inhibit-startup-message t) 
-
-;; no tool bar and scroll bar
-(tool-bar-mode -1)
-(when (display-graphic-p)
-  (scroll-bar-mode -1))
-
-;; no tab indent
-(setq-default indent-tabs-mode nil)
+;; Basic set up
+(global-linum-mode t) ;; line number
+(global-hl-line-mode t) ;; highlight current line
+(setq inhibit-startup-message t) ;; no startup msg  
+(tool-bar-mode -1) ;; no tool bar and scroll bar
+(scroll-bar-mode -1) ;; no scroll bar
+(setq-default indent-tabs-mode nil) ;; no tab indent
 
 (setq-default c-basic-offset 4)
-
 (setq python-indent-offset 4)
 
-;; split vertically
-(setq split-width-threshold 0)
+(setq split-width-threshold 0) ;; split vertically
 (setq split-height-threshold nil)
 
 ;; load-path
@@ -32,8 +20,8 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; Shell mode
-(setq explicit-shell-file-name "bash")
-(setq explicit-bash-args '("-i" "-l"))
+(setq explicit-shell-file-name "zsh")
+(setq explicit-bash-args '("-l", "-i"))
 
 ;; Backup and auto saving setting
 (setq auto-save-file-name-transforms `((".*" , "~/.emacs.temp/" t)))
@@ -47,7 +35,7 @@
 
 ;; Solarized theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/theme")
-(if (display-graphic-p) (load-theme 'solarized t))
+(load-theme 'solarized t)
 
 (require 'evil)
 (require 'init-smex)
@@ -56,10 +44,7 @@
 (require 'init-editing)
 (require 'init-keybinding)
 (require 'window-numbering)
-
-;; Set C-h to backspace
-;; Set C-d to help
-;(global-set-key (kbd "C-h") 'delete-backward-char)
+(window-numbering-mode 1)
 
 
 ;; MELPA packages
@@ -68,7 +53,7 @@
              '("melpa" . "http://melpa.org/packages/")
              t)
 
-;; elpy and python
+;; python
 ;(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 (setq python-shell-interpreter "/Users/Armstrong/anaconda/bin/python")
 ;      python-shell-interpreter-args "-i")
@@ -78,14 +63,6 @@
 (add-hook 'python-mode-hook
   (lambda ()
     (setq python-indent 4)))
-
-;(with-eval-after-load "csv-mode" (csv-align-fields 1 1 0))
-
-;; Company mode set key
-;(with-eval-after-load 'company
-;  (define-key company-active-map (kbd "C-h") 'delete-backward-char))
-;(with-eval-after-load 'company
-;  (define-key company-active-map (kbd "<deletechar>") 'company-show-doc-buffer))
 
 ;; Configuration for org-mode
 (org-babel-load-file "~/.emacs.d/org_config.org")
@@ -97,7 +74,6 @@
 
 ;; Font setting
 (set-frame-font "Menlo 18" nil t)
-;;(set-frame-font "Source Code Pro 16" nil t)
 
 ;; Path Setting for SML, Racket, Homebrew
 ;; http://ergoemacs.org/emacs/emacs_env_var_paths.html
@@ -109,7 +85,7 @@
                           "/Applications/Racket v6.10/bin"
                            "/usr/local/bin")
                           exec-path))
-;;
+
 
 ;; Markdown Mode
 ;; Attention, this package is manually added, so you have to update it manually if you want
